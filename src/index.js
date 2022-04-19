2632c9a284c31353c26ca08ffa4d7479a3f86bb3
Oybek <oybekjonxayrullayevich@gmail.com>
2022-04-19 10:43:07 +0500

    react starter

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = function () {
+  const text = "ismni kiriting";
+  const styleForm = { color: "red", backgroundColor: "yellow" };
+  return (
+    <div>
+      <label htmlFor="name">{text}</label>
+      <input type={text} id="name" />
+      <button style={styleForm}>Send</button>
+    </div>
+  );
+};
+ReactDOM.render(<App />, document.querySelector("#root"));
